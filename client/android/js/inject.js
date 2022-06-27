a9d78b8a76914948704a3d6af72322cce7012ab6
Ujjwal Kashyap <mr.ujjwalg@gmail.com>
2022-06-28 02:02:10 +0530

    Update inject.js

--- client/android/js/inject.js
+++ client/android/js/inject.js
@@ -67,13 +67,14 @@ const alist = document.querySelectorAll("div.items.png");
     for (let i = 0; i < alist.length; i++) {
       alist[i].style.setProperty("height", "auto", "important");
       let child = alist[i].querySelectorAll("a");
-      if (child.length ===3) {
-           alist[i].style.setProperty("overflow", "scroll", "important");
+      if (child.length % 2 != 0) {
+           alist[i].style.setProperty("overflow-x", "scroll", "important");
       for (let c = 0; c < child.length; c++) {
           child[c].parentNode.style.setProperty("flex-wrap", "unset", "important");
           child[c].parentNode.style.setProperty("justify-content", "unset", "important");
-          child[c].querySelector("img").style.height="100px";
-          child[c].querySelector("img").style.width="120px";
+          child[c].querySelector("img").style.height="75%";
+          child[c].querySelector("img").style.width="75%";
+          child[c].querySelector("img").style.setProperty("object-fit", "cover", "important");
           child[c].querySelector("img").style.setProperty("max-width", "unset", "important");
       }
     }
