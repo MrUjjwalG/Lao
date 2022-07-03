cdd987ef8463f5c6698c97fcc3b810f04545d140
Ujjwal Kashyap <mr.ujjwalg@gmail.com>
2022-07-03 16:45:01 +0530

    Update inject.js

--- client/android/js/inject.js
+++ client/android/js/inject.js
@@ -30,9 +30,15 @@ if (document.body.offsetWidth > 768) {
   const ulist = document.querySelectorAll("i.iconf.ti-shopping-cart-full")[8];
 
   const logoutBtn = document.querySelector("#ctl00_lnkSignOut");
+  const logoutBtnS document.querySelector("#ctl00_lnkSignOut1")
 
   if (logoutBtn) {
     logoutBtn.onclick = function () {
+      Android.logOut();
+    };
+  }
+  if (logoutBtnS) {
+    logoutBtnS.onclick = function () {
       Android.logOut();
     };
   }
