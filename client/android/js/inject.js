a949fc207e169f4073a542fab29c11b912522e69
Ujjwal Kashyap <mr.ujjwalg@gmail.com>
2022-07-20 13:38:44 +0530

    Update inject.js

--- client/android/js/inject.js
+++ client/android/js/inject.js
@@ -1,3 +1,6 @@
+if (window.location.href === "https://www.laoapp.in/Default.aspx?aspxerrorpath=/Default.aspx") {
+        Android.logOut();
+    }
 if (document.body.offsetWidth > 768) {
     var Str =
         '<!DOCTYPE html> <html> <head> <title>Check if an element is a div in JavaScript.</title> </head> <body style="text-align: center; background-color: black"> <div style="overflow: hidden; display: flex; justify-content: center"> <h2 style="color: red; float: left">LAO </h2> <h2 style="color: white; float: left">APP</h2> </div> <p style="color: white">Your device does not support LAO APP</p> </body> </html>';
@@ -173,8 +176,5 @@ if (document.body.offsetWidth > 768) {
             imgsc[i].style.setProperty("width", "50%", "important");
             imgsc[i].style.setProperty("padding", "25px", "important");
         }
-    }
-    else if (window.location.href === "https://www.laoapp.in/Default.aspx?aspxerrorpath=/Default.aspx") {
-        Android.logOut();
     }
 }
