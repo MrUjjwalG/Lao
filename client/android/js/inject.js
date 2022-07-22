98c5cc59aef5e59979ccbdf63f73edeacf0d7710
Ujjwal Kashyap <mr.ujjwalg@gmail.com>
2022-07-22 19:35:26 +0530

    Update inject.js

--- client/android/js/inject.js
+++ client/android/js/inject.js
@@ -91,9 +91,9 @@ if (document.body.offsetWidth > 768) {
     } else if (window.location.href.includes("https://www.laoapp.in/ResetPassword.aspx")) {
         document.querySelector("#ctl00_ContentPlaceHolder1_lnkOtpVerify").classList.remove("btn-fill-out");
     }else if (window.location.href.includes("https://www.laoapp.in/custRegistration.aspx")) {
-        document.querySelector("#ctl00_ContentPlaceHolder1_lnkRegisterBtn").style.color="#fff";
+        document.querySelector("#ctl00_ContentPlaceHolder1_lnkRegisterBtn").style.setProperty("color", "#fff", "important");
     }else if (window.location.href.includes("https://www.laoapp.in/VerifiyOTP.aspx")) {
-        document.querySelector("#ctl00_ContentPlaceHolder1_lnkOtpVerify").style.color="#fff";
+        document.querySelector("#ctl00_ContentPlaceHolder1_lnkOtpVerify").style.setProperty("color", "#fff", "important");
     }
     if (
         window.location.href === "https://www.laoapp.in/" ||
