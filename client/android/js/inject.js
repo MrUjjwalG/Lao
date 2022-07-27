7f5243232c3b42edbfc97cbfadbc831a0cdabc53
Ujjwal Kashyap <mr.ujjwalg@gmail.com>
2022-07-27 08:41:52 +0530

    Update inject.js

--- client/android/js/inject.js
+++ client/android/js/inject.js
@@ -30,15 +30,17 @@ if (document.body.offsetWidth > 768) {
     if (var3) {
         var3.style.setProperty("margin-top", "10px", "important");
     }
-    
+
     var searchBox = $("#ctl00_TextBox1")[0];
-    if(searchBox){
-        searchBox.type="search";
-        searchBox.on("keydown", "form", function(event) { 
-        if(event.key == "Enter"){
-           if (document.activeElement instanceof HTMLElement){
-                document.activeElement.blur();
-           }}});
+    if (searchBox) {
+        searchBox.type = "search";
+        searchBox.on("keydown", "form", function (event) {
+            if (event.key == "Enter") {
+                if (document.activeElement instanceof HTMLElement) {
+                    document.activeElement.blur();
+                }
+            }
+        });
     }
 
     var ulist = document.querySelectorAll("i.iconf.ti-shopping-cart-full")[8];
